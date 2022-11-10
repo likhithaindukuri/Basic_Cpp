25ece822bcb403c3f006ea6b62f88e3c19a7d14a
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2022-11-10 18:52:32 +0530

    detect_loop

---
+++ detectloop.cpp
@@ -0,0 +1,12 @@
+if(head==NULL || head->next==NULL)
+            return false;
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                return true;
+        }
+        return false;
