d0e06b076874cfc116cff4aa4cc0bc4d908a82bf
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2022-12-15 20:09:33 +0530

    list

---
+++ rd.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+Node *removeDuplicates(Node *head)
+{
+ Node* current = head;
+    while(current && current->next){
+        if(current->next->data == current->data){
+            Node* tobeDel = current->next;
+            current->next = current->next->next;
+            delete tobeDel;
+        }else{
+            current = current->next;
+        }
+    }
+    return head;
+}
